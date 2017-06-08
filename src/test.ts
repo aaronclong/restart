924b4e4074bbf39e2b4b5104f62464f0ce3dcb32
Aaron Long <aaron.long.c@gmail.com>
2017-06-08 15:28:25 -0400

    Added testing

--- src/test.ts
+++ src/test.ts
@@ -6,11 +6,25 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-import { getTestBed } from '@angular/core/testing';
-import {
+
+// reference url: http://lathonez.com/2017/ionic-2-unit-testing/
+import { getTestBed, TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { App, 
+         Config,
+         Form,
+         IonicModule,
+         Keyboard,
+         DomController,
+         MenuController,
+         NavController,
+         Platform } from 'ionic-angular';
+import { ConfigMock, PlatformMock } from './test-utils/mocks';
+
+/* import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+} from '@angular/platform-browser-dynamic/testing'; */
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare const __karma__: any;
@@ -30,3 +44,33 @@ const context = require.context('./', true, /\.spec\.ts$/);
 context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
+
+export class TestUtils {
+
+  public static beforeEachCompiler(components: Array<any>): Promise<{fixture: any, instance: any}> {
+    return TestUtils.configureIonicTestingModule(components)
+      .compileComponents().then(() => {
+        let fixture: any = TestBed.createComponent(components[0]);
+        return {
+          fixture: fixture,
+          instance: fixture.debugElement.componentInstance,
+        };
+      });
+  }
+
+  public static configureIonicTestingModule(components: Array<any>): typeof TestBed {
+    return TestBed.configureTestingModule({
+      declarations: [
+        ...components,
+      ],
+      providers: [
+        App, Form, Keyboard, DomController, MenuController, NavController,
+        {provide: Platform, useClass: PlatformMock},
+        {provide: Config, useClass: ConfigMock},
+      ],
+      imports: [
+        IonicModule
+      ],
+    });
+  }
+}
