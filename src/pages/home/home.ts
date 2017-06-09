0a580887f22e25e28993e8b836149948d203bee7
Jarryd Stamatelos <jarryd.stamatelos@gmail.com>
2017-06-08 22:35:44 -0400

    styles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,7 @@ import { PartnerSignUp } from '../partnersignup/partnersignup';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    
   }
   goToVolunteerSignUp(){
     this.navCtrl.push(VolunteerSignUp);
